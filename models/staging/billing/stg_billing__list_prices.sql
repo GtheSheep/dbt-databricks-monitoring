7bab1adb0160fe64240456c53dc8ea4f2f9c2a1d
Gary James <13685708+GtheSheep@users.noreply.github.com>
2024-06-23 10:50:03 +0100

    fix - Remove increment from stg_billing__list_prices.sql

--- models/staging/billing/stg_billing__list_prices.sql
+++ models/staging/billing/stg_billing__list_prices.sql
@@ -1,6 +1,6 @@
 {{
   config(
-    materialized='incremental',
+    materialized='table',
     unique_key='list_price_id'
   )
 }}
@@ -16,9 +16,5 @@ select
   {{ dbt_utils.generate_surrogate_key(['account_id', 'price_start_time', 'price_end_time', 'sku_name', 'cloud', 'currency_code', 'usage_unit']) }} as list_price_id
 from
   {{ source('databricks_billing', 'list_prices') }}
-{% if is_incremental() %}
-where
-  price_start_time >= (select coalesce(max(price_start_time), '1970-01-01'::date) from {{ this }})
-{% endif %}
 order by
   price_start_time asc
