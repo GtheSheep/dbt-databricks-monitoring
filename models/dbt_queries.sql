ca3fa490c82cf051ccb2fed13a2989c426a92c0b
Gary James <13685708+GtheSheep@users.noreply.github.com>
2024-11-21 12:11:47 +0000

    fix - statement_id

--- models/dbt_queries.sql
+++ models/dbt_queries.sql
@@ -6,6 +6,7 @@
 }}
 with dbt_queries as (
   select 
+    statement_id,
     statement_text,
     total_duration_ms,
     start_time,
@@ -23,6 +24,7 @@ with dbt_queries as (
 ),
 final as (
   select
+    dbt_queries.statement_id,
     dbt_queries.statement_text,
     dbt_queries.start_time,
     dbt_queries.end_time,
@@ -33,7 +35,6 @@ final as (
     dbt_queries.dbt_metadata:dbt_databricks_version::string as dbt_databricks_version,
     dbt_queries.dbt_metadata:target_name::string as target_name,
     dbt_queries.dbt_metadata:node_id::string as node_id,
-    usage_spend.statement_id,
     usage_spend.spend * dbt_queries.total_duration_ms / 3600000 as query_spend
   from
     {{ ref('usage_spend') }} -- needs an increment
